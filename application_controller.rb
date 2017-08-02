e91a2e90a84caab10b1cf0634ecb0814a886e3a7
Arianna <amoore@cristoreyny.org>
2017-08-02 15:14:35 +0000

    Project Working!

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,23 @@
 require 'bundler'
 Bundler.require
 
+require_relative './models/dog.rb'
+
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  
+  get "/dog" do
+    @dog1 = Dog.new("Fiji","Kab", 5)
+    erb :dog
+    end
 
-end
+get "/info" do
+  erb :info
+  end
+  get "/color" do
+    erb :color
+  end 
+end 
---
+++ models/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+    attr_accessor :age, :name, :breed
+    
+    def intialize(age,name,breed)
+        @name = name
+        @age = age
+        @breed = breed
+    end
+end 
